e9a1728178ee2be7172b79aaaf5040d5afe82e8f
taviur <tavia.davies@hotmail.co.uk>
2015-11-09 20:19:49 +0000

    fix .css links

---
+++ myscript.js
@@ -0,0 +1,10 @@
+$( "p" ).css( "border", "9px solid red" );
+
+$( "p" ).click(function() {
+  alert( "Handler for .click() called." );
+});
+
+$( "h1" ).click(function() {
+$( this ).css( "color", "green" );
+} );
+ 
